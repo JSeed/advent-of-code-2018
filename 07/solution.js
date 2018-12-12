ce70e944059c3bef83043414953c14d8fead0fcb
Jon Seed <jonathan.j.seed@gmail.com>
2018-12-12 18:02:31 +0000

    Day 7, Part 1

--- 07/solution.js
+++ 07/solution.js
@@ -1,4 +1,4 @@
-const input = require('./sample.json');
+const input = require('./input.json');
 
 const parseInput = (str) => {
     const matches = str.match("Step (.) must be finished before step (.) can begin.");
@@ -7,4 +7,42 @@ const parseInput = (str) => {
 
 const requirements = input.map(parseInput);
 
-console.log(requirements);
+const stepDependencies = {};
+const dependentSteps = {};
+
+function add(map, key, value) {
+    if(!map[key]) map[key] = new Set();
+    if(!map[value]) map[value] = new Set();
+    map[key].add(value);
+}
+
+const steps = new Set();
+
+for(let {step, dependency} of requirements) {
+    if(!steps.has(step))steps.add(step);
+    if(!steps.has(dependency)) steps.add(dependency);
+    add(stepDependencies, step, dependency);
+    add(dependentSteps, dependency, step);
+}
+
+// Initialize the lists of ready and done steps
+const ready = [...steps].filter((step) => stepDependencies[step].size === 0); // List of all steps with 0 dependencies
+const done = [];
+
+while(ready.length > 0) {
+    // Get the next step
+    const step = ready.sort((a, b) => a.localeCompare(b)).shift();
+
+    // Remove from dependencies of dependents
+    dependentSteps[step].forEach((dependent) => {
+        stepDependencies[dependent].delete(step)
+
+        // If the dependent step has no more dependencies, mark it as ready
+        if (stepDependencies[dependent].size == 0) ready.push(dependent);
+    });
+
+    // Add step to completed
+    done.push(step);
+}
+
+console.log(done.join(''));
